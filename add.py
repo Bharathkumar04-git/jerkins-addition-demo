0235fb8448843c2d3b9b4eeaab70558c374d57a3
Bharath kumar <bharathkumar42006@gmail.com>
2026-08-27 22:07:39 +0530

    first commit

---
+++ add.py
@@ -0,0 +1,15 @@
+import sys
+
+def add_numbers(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    num1 = int(input("Enter the first number: "))
+    num2 = int(input("Enter the second number: "))
+    result = add_numbers(num1, num2)
+    print("=================================")
+    print("Addition Result")
+    print("=================================")
+    print(f"First Number : {num1}")
+    print(f"Second Number: {num2}")
+    print(f"Sum : {result}")
